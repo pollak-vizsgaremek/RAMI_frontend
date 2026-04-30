6754976169397bd420943b514836a9ec2f60464c
MLevi468 <mlevente468@gmail.com>
2026-04-30 11:11:17 +0200

    Fix test

--- playwright.config.js
+++ playwright.config.js
@@ -39,12 +39,6 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
